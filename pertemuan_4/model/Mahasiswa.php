8a6f6c58c147087586da6dd002dbceb767f6e236
Rieyani <gunangeh7@gmail.com>
2023-11-06 20:28:12 +0900

    buat pertemuan 4

---
+++ pertemuan_4/model/Mahasiswa.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once "apps/Database.php";
+
+class Mahasiswa {
+    private $db;
+
+    
+    public function __construct(){
+        $this->db = new Database();
+    }
+    public function getAll(){
+        $sql = $this->db->con->prepare("SELECT * FROM mahasiswa");
+        $sql->execute();
+
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function showText($text=""){
+        echo "- $text <br>";
+    }
+
+    public function showAll(){
+        foreach($this->getAll() as $d){
+            echo "Nama : " . $d['nama'] .  "<br>";
+            echo "NIM : " . $d['nim'] .  "<br>";
+            echo "<br><br>";
+        }
+    }
+}
