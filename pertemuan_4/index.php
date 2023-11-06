5b9c8dac367674d5c13dc0c1232984a16b266149
Rieyani <gunangeh7@gmail.com>
2023-11-06 22:35:33 +0900

    bnuaat kecepaaatan

--- pertemuan_4/index.php
+++ pertemuan_4/index.php
@@ -2,13 +2,27 @@
 
 include_once "model/Mahasiswa.php";
 include_once "model/Mobil.php";
+include_once "model/Robot.php";
 
 
-"
-- buat sebuah class baru
-- dengan nama Robot
-- property : nama, warnah, kecepatan
-- method : tampilBiodata, lihatKecepatan, tambahKecepatan
+$transformer = new Robot("Transformar"); 
 
 
-";
+$transformer->warna = "Kuning" ;
+
+$transformer->tampilBiodata();
+
+$transformer->tambahKecepatan(20);
+$transformer->tambahKecepatan(10);
+$transformer->tambahKecepatan(5);
+
+$transformer->kurangiKecepatan(30);
+$transformer->tambahKecepatan(50);
+
+
+
+
+
+$transformer->turboKecepatan();
+
+
--- pertemuan_4/model/Robot.php
+++ pertemuan_4/model/Robot.php
@@ -1,3 +1,43 @@
 <?php
 
-class Robot {}
+class Robot {
+    public  $warna;
+
+    private $nama, $kecepatan;
+
+
+    public function __construct($nama){
+        $this->nama = $nama;
+    }
+
+
+    public function turboKecepatan(){
+        $this->kecepatan = $this->kecepatan * 2 ;
+    }
+
+    public function hitungLuas($panjang, $lebar){
+        $this->showText("Nilai Pajang : $panjang");
+        $this->showText("Nilai Lebar : $lebar");
+
+        $luas = $panjang * $lebar;
+
+        $this->showText("Hasil Luas : $luas");
+        
+    }
+
+    public function kurangiKecepatan($kecepatanBerkurang){
+        $this->kecepatan -= $kecepatanBerkurang;
+    }
+    public function tambahKecepatan($kecepatan){
+        $this->kecepatan += $kecepatan;
+    }
+    public function tampilBiodata(){
+        
+        $this->showText("Nama Robot : $this->nama");
+        $this->showText("Warnah Robot : $this->warna");
+        $this->showText("Kecepatan Robot : $this->kecepatan");
+    }
+    private function showText($text=""){
+        echo "- $text <br>";
+    }
+}
