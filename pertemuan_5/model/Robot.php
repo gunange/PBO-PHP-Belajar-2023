e0eb0d92eb4718e476eeb8c93738c765e748a421
Rieyani <gunangeh7@gmail.com>
2023-11-07 20:29:37 +0900

    chalange pertemuan 5

---
+++ pertemuan_5/model/Robot.php
@@ -0,0 +1,52 @@
+<?php
+
+class Robot {
+    public  $warna;
+
+    private $nama, $kecepatan;
+
+
+    public function __construct($nama){
+        $this->nama = $nama;
+    }
+
+
+    public function turboKecepatan(){
+        $this->kecepatan = $this->kecepatan * 2 ;
+    }
+
+    public function tambah($nilai_a, $nilai_b){
+        $this->showText("Nilai A : $nilai_a");
+        $this->showText("Nilai B : $nilai_b");
+
+        $hasil = $nilai_a + $nilai_b;
+
+        $this->showText("Hasil : $hasil");
+    }
+
+    public function hitungLuas($panjang, $lebar){
+        $this->showText("Nilai Pajang : $panjang");
+        $this->showText("Nilai Lebar : $lebar");
+
+        $luas = $panjang * $lebar;
+
+        $this->showText("Hasil Luas : $luas");
+        
+    }
+
+    public function kurangiKecepatan($kecepatanBerkurang){
+        $this->kecepatan -= $kecepatanBerkurang;
+    }
+    public function tambahKecepatan($kecepatan){
+        $this->kecepatan += $kecepatan;
+    }
+    public function tampilBiodata(){
+        
+        $this->showText("Nama Robot : $this->nama");
+        $this->showText("Warnah Robot : $this->warna");
+        $this->showText("Kecepatan Robot : $this->kecepatan");
+    }
+    private function showText($text=""){
+        echo "- $text <br>";
+    }
+}
